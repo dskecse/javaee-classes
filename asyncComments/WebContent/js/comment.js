74f997d5f02e8d7955ee9a52346633546dfd9e99
Dzianis Dashkevich <dskecse@gmail.com>
2014-01-13 13:17:35 +0300

    Make ajax requests work

---
+++ asyncComments/WebContent/js/comment.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    $.post('comment', $(this).serialize(), function (response) {
+      $('#message').text(response).show().delay(1500).fadeOut(800);
+    });
+  });
+});
